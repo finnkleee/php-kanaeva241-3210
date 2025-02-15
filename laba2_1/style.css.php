856d69ac4f962e5292ae9ae7f7953dc644da8df3
finnkleee <e.n.nikitushkina@stud.mospolytech.ru>
2025-02-15 10:56:15 +0300

    2

---
+++ laba2_1/style.css.php
@@ -0,0 +1,21 @@
+<?php
+    header('content-type: text/css');
+?>
+
+.header__wrapper{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+}
+.zadanie {
+    text-align: center;
+    margin: 0 auto;
+}
+
+.main__wrapper{
+    margin: 100px auto 100px 50px
+}
+.foot__text{
+    text-align: center;
+}
