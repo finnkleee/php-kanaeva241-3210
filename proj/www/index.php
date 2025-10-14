f45c575f3779dd3c4a8b975f86ea35f620da7877
finnkleee <blurnik43@gmail.com>
2025-10-14 19:43:20 +0300

    proj namespace + auto

---
+++ proj/www/index.php
@@ -0,0 +1,20 @@
+<?php
+ 
+    spl_autoload_register(function (string $className) {
+
+    require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
+
+    });
+
+    $author =  new \MyProject\Models\Users\User('Иван');
+
+    $article = new \MyProject\Models\Articles\Article('Заголовок', 'Текст', $author);
+    var_dump($article);
+?>
+/* <?php
+spl_autoload_register(function(string $className) {
+    require_once dirname(__DIR__).'\\'.$className.'.php';
+});
+$controller = new \src\Controllers\MainController();
+$controller->sayHello();
+$route = $_GET['route'] ?? '';
