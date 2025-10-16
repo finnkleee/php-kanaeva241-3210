a4ee3b167370925214e0973f16ea7fc5feb763db
finnkleee <blurnik43@gmail.com>
2025-10-16 20:12:48 +0300

    add

--- proj/src/MyProject/Models/Articles/Article.php
+++ proj/src/MyProject/Models/Articles/Article.php
@@ -38,5 +38,10 @@ class Article extends ActiveRecordEntity
     {
         return User::getById($this->authorId);
     }
+
+    public function setAuthor(User $author): void
+    {
+        $this->authorId = $author->getId();
+    }
 }
 ?>
--- proj/www/routes.php
+++ proj/www/routes.php
@@ -7,6 +7,7 @@ return [
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
     '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
     '~^articles/(\d+)/edit$~' => [\MyProject\Controllers\ArticlesController::class, 'edit'],
+    '~^articles/add$~' => [\MyProject\Controllers\ArticlesController::class, 'add'],
 
 ];
 
